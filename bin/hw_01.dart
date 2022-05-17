811d94369f48756b70e6ea379f25b58e19898c81
lamia-x5 <lamia-abdullah@outlook.com>
2022-05-17 22:18:49 +0300

    first home work with dart

---
+++ bin/hw_01.dart
@@ -0,0 +1,13 @@
+void main() {
+  //my name
+  const name = "lamia";
+  /* date 
+  of 
+  birth */
+  var birth = 1997;
+  //mobile number
+  var number = "555987654";
+  print("my name is $name ,date of birth $birth , mobile number $number");
+  print(number[3]);
+  print(name.length);
+}

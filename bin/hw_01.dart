bf4cbebd4a96a2ff599e508309a69ff20f426486
Lamia Abdullah <89685293+lamia-x5@users.noreply.github.com>
2022-05-18 22:41:08 +0300

    Update hw_01.dart
    add list and remove

--- bin/hw_01.dart
+++ bin/hw_01.dart
@@ -10,4 +10,19 @@ void main() {
   print("my name is $name ,date of birth $birth , mobile number $number");
   print(number[3]);
   print(name.length);
+  
+  // list
+  var colors = ["green", "grey", "white", "black"];
+  print(colors);
+  // remove grey 
+  var removedItem = colors.remove("grey");
+  print(colors);
+  print(removedItem);
+  // removeLast = black
+  var removItem = colors.removeLast();
+  print(colors);
+  print(removedItem);
+  
+  
+  
 }
